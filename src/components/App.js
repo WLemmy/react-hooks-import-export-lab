43c7fc9a067f760d9ba94053a9c147ca9ed070b9
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-04-26 05:13:27 +0300

    Import and Export

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { NavBar} from "./components/navbar.js";
+import { Home } from "./components/home.js";
+import { About } from "./components/about.js";
 function App() {
   return (
     <div>
--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,29 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+import React from "react";
+
+export function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+export function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      <p>This is the about page content.</p>
+    </div>
+  );
+}
